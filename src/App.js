005cb01cde82ccd539eca252eb7f19eab1a47a4d
jiminjesu <contact@theleadingcrew.io>
2021-12-05 23:28:00 +0800

    Some updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {
 	getCurrentWalletConnected,
 	mint,
 } from "./interact.js";
-import gif from './promogif3.gif';
+import gif from './img/promogif3.gif';
+import logo from './img/tlc_logo_black.png';
 
 require("dotenv").config();
 
@@ -16,103 +17,91 @@ const App = () => {
 	const [mintAmount, setMintAmount] = useState("");
 
 	useEffect(async () => {
-	  const { address, status } = await getCurrentWalletConnected();
+	const { address, status } = await getCurrentWalletConnected();
 
-	  setWallet(address);
-	  setStatus(status);
+	setWallet(address);
+	setStatus(status);
 
-	  addWalletListener();
+	addWalletListener();
 	}, []);
 
 	function addWalletListener() {
-	  if (window.ethereum) {
-		window.ethereum.on("accountsChanged", (accounts) => {
-		  if (accounts.length > 0) {
-			setWallet(accounts[0]);
-			setStatus("Check that you are connected to the Ethereum Mainnet.");
-		  } else {
-			setWallet("");
-			setStatus("🦊 Connect to Metamask using the top right button.");
-		  }
-		});
-	  } else {
-		setStatus(
-		  <p>
-			{" "}
-			🦊{" "}
-			<a target="_blank" href={`https://metamask.io/download.html`}>
-			  You must install Metamask, a virtual Ethereum wallet, in your
-			  browser.
-			</a>
-		  </p>
-		);
-	  }
+		if (window.ethereum) {
+			window.ethereum.on("accountsChanged", (accounts) => {
+			if (accounts.length > 0) {
+				setWallet(accounts[0]);
+				setStatus("Check that you are connected to the Ethereum Mainnet.");
+			} else {
+				setWallet("");
+				setStatus("🦊 Connect to Metamask using the top right button.");
+			}
+			});
+		} else {
+			setStatus(
+			<p>
+				{" "}
+				🦊{" "}
+				<a target="_blank" href={`https://metamask.io/download.html`}>
+				You must install Metamask, a virtual Ethereum wallet, in your
+				browser.
+				</a>
+			</p>
+			);
+		}
 	}
 
 	const connectWalletPressed = async () => {
-	  const walletResponse = await connectWallet();
-	  setStatus(walletResponse.status);
-	  setWallet(walletResponse.address);
+		const walletResponse = await connectWallet();
+		setStatus(walletResponse.status);
+		setWallet(walletResponse.address);
 	};
 
 	const onMintPressed = async () => {
-	  const { success, status } = await mint(mintAmount);
-	  setStatus(status);
-	  if (success) {
-		setMintAmount("");
-	  }
+		const { success, status } = await mint(mintAmount);
+		setStatus(status);
+		if (success) {
+			setMintAmount("");
+		}
 	};
 
 	return (
-		walletAddress.length > 0
-		?
+		walletAddress.length > 0 ?
 		(
-		<div className="Minter">
-		<button id="walletButton">
-		{
-		"Connected: " +
-		  String(walletAddress).substring(0, 6) +
-		  "..." +
-		  String(walletAddress).substring(38)
-		}
-	  	</button>
-		{"Contract address: " + String(process.env.REACT_APP_CONTRACT_ADDRESS).substring(0, 6) + "..." +	String(process.env.REACT_APP_CONTRACT_ADDRESS).substring(38)}
+			<div className="Minter">
+				<button id="walletButton">
+					{"Connected: " + String(walletAddress).substring(0, 6) + "..." + String(walletAddress).substring(38)}
+				</button>
+				<img src={logo} width="75" height="75" />
+				{"Contract address: " + String(process.env.REACT_APP_CONTRACT_ADDRESS).substring(0, 6) + "..." +	String(process.env.REACT_APP_CONTRACT_ADDRESS).substring(38)}
 
-		<h1 id="title">Mint TheLeadingCrew NFT</h1>
-		<img src={gif} alt="Logo" width="400" height="400" />
+				<h1 id="title">MINT THE LEADING CREW NFT</h1>
+				<img src={gif} alt="Logo" width="400" height="400" />
+				<form>
+					<p>Each NFT cost 0.06 ETH (excluding gas fees). </p>
+					<p>Enter the amount of NFTs you wish to mint and press "Mint".</p>
+					<input
+						type="text"
+						placeholder="Enter mint amount: 1 to 6 (Max)"
+						onChange={(event) => setMintAmount(event.target.value)}
+					/>
+				</form>
 
-		<form>
-		<p>Enter the amount of NFTs you wish to mint and press "Mint"</p>
-		<p>Each NFT cost 0.06 ETH </p>
-        <input
-          type="text"
-          placeholder="Enter mint amount: 1 to 6 (Max)"
-          onChange={(event) => setMintAmount(event.target.value)}
-        />
-      </form>
-
-		<button id="mintButton" onClick={onMintPressed}>
-			Mint
-		</button>
-		<p id="status" style={{ color: "red" }}>
-		  {status}
-		</p>
-	  </div>
-		)
-		:
+				<button id="mintButton" onClick={onMintPressed}>
+					Mint
+				</button>
+				<p id="status" style={{ color: "red" }}> {status} </p>
+			</div>
+		) :
 		(
-		<div className="Minter">
-		<button id="connectButton" onClick={connectWalletPressed}>
-		  {<span>Connect Wallet</span>}
-		</button>
-		<br></br>
-		<br></br>
-		{"Contract address: " + String(process.env.REACT_APP_CONTRACT_ADDRESS).substring(0, 6) + "..." +	String(process.env.REACT_APP_CONTRACT_ADDRESS).substring(38)}
-		<h1 id="title">The Leading Crew NFT Minter</h1>
-		<img src={gif} alt="Logo" width="400" height="400" />
-		</div>
+			<div className="Minter">
+				<img src={logo} width="400" height="400" />
+				<br></br>
+				<button id="connectButton" onClick={connectWalletPressed} height="70">
+					{<span>Connect Wallet</span>}
+				</button>
+			</div>
 		)
 	);
-  };
+};
 
 export default App;
--- src/interact.js
+++ src/interact.js
@@ -7,124 +7,124 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
 export const connectWallet = async () => {
-  if (window.ethereum) {
-    try {
-      const addressArray = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      const obj = {
-        status: "Check that you are connected to the Ethereum Mainnet.",
-        address: addressArray[0],
-      };
-      return obj;
-    } catch (err) {
-      return {
-        address: "",
-        status: "😥 " + err.message,
-      };
-    }
-  } else {
-    return {
-      address: "",
-      status: (
-        <span>
-          <p>
-            {" "}
-            🦊{" "}
-            <a target="_blank" href={`https://metamask.io/download.html`}>
-              You must install Metamask, a virtual Ethereum wallet, in your
-              browser.
-            </a>
-          </p>
-        </span>
-      ),
-    };
-  }
+	if (window.ethereum) {
+		try {
+			const addressArray = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+			const obj = {
+				status: "Check that you are connected to the Ethereum Mainnet.",
+				address: addressArray[0],
+			};
+			return obj;
+		} catch (err) {
+			return {
+				address: "",
+				status: "😥 " + err.message,
+			};
+		}
+	} else {
+		return {
+			address: "",
+			status: (
+				<span>
+				<p>
+					{" "}
+					🦊{" "}
+					<a target="_blank" href={`https://metamask.io/download.html`}>
+					You must install Metamask, a virtual Ethereum wallet, in your
+					browser.
+					</a>
+				</p>
+				</span>
+			),
+		};
+	}
 };
 
 export const getCurrentWalletConnected = async () => {
-  if (window.ethereum) {
-    try {
-      const addressArray = await window.ethereum.request({
-        method: "eth_accounts",
-      });
-      if (addressArray.length > 0) {
-        return {
-          address: addressArray[0],
-          status: "Check that you are connected to the Ethereum Mainnet.",
-        };
-      } else {
-        return {
-          address: "",
-          status: "🦊 Connect to Metamask using the top right button.",
-        };
-      }
-    } catch (err) {
-      return {
-        address: "",
-        status: "😥 " + err.message,
-      };
-    }
-  } else {
-    return {
-      address: "",
-      status: (
-        <span>
-          <p>
-            {" "}
-            🦊{" "}
-            <a target="_blank" href={`https://metamask.io/download.html`}>
-              You must install Metamask, a virtual Ethereum wallet, in your
-              browser.
-            </a>
-          </p>
-        </span>
-      ),
-    };
-  }
+	if (window.ethereum) {
+		try {
+			const addressArray = await window.ethereum.request({
+				method: "eth_accounts",
+			});
+			if (addressArray.length > 0) {
+				return {
+				address: addressArray[0],
+				status: "Check that you are connected to the Ethereum Mainnet.",
+				};
+			} else {
+				return {
+				address: "",
+				status: "🦊 Connect to Metamask using the top right button.",
+				};
+			}
+		} catch (err) {
+			return {
+				address: "",
+				status: "😥 " + err.message,
+			};
+		}
+	} else {
+		return {
+			address: "",
+			status: (
+				<span>
+				<p>
+					{" "}
+					🦊{" "}
+					<a target="_blank" href={`https://metamask.io/download.html`}>
+					You must install Metamask, a virtual Ethereum wallet, in your
+					browser.
+					</a>
+				</p>
+				</span>
+			),
+		};
+	}
 };
 
 export const mint = async (mintAmount) => {
 	if (mintAmount.trim() == "") {
-	  return {
-		success: false,
-		status: "❗Please make sure all fields are completed before minting.",
-	  };
+		return {
+			success: false,
+			status: "❗Please make sure all fields are completed before minting.",
+		};
 	}
 	if (mintAmount > 6) {
 		return {
-		  success: false,
-		  status: "You can only mint a maximum of 6 NFTs.",
+		success: false,
+		status: "You can only mint a maximum of 6 NFTs.",
 		};
-	  }
+	}
 
 	window.contract = await new web3.eth.Contract(contractABI, contractAddress);
 
 	const transactionParameters = {
-	  to: contractAddress, // Required except during contract publications.
-	  from: window.ethereum.selectedAddress, // must match user's active address.
-	  value: (Number(6e16)* mintAmount).toString(16),
-	  data: window.contract.methods
+	to: contractAddress, // Required except during contract publications.
+	from: window.ethereum.selectedAddress, // must match user's active address.
+	value: (Number(6e16)* mintAmount).toString(16),
+	data: window.contract.methods
 		// .publicMint(mintAmount)
 		.presaleMint(mintAmount)
 		.encodeABI(),
 	};
 
-  try {
-    const txHash = await window.ethereum.request({
-      method: "eth_sendTransaction",
-      params: [transactionParameters],
-    });
-    return {
-      success: true,
-      status:
-        "✅ Check out your transaction on Etherscan: https://etherscan.io/tx/" +
-        txHash,
-    };
-  } catch (error) {
-    return {
-      success: false,
-      status: "😥 Something went wrong: " + error.message,
-    };
-  }
+	try {
+		const txHash = await window.ethereum.request({
+		method: "eth_sendTransaction",
+		params: [transactionParameters],
+		});
+		return {
+		success: true,
+		status:
+			"✅ Check out your transaction on Etherscan: https://etherscan.io/tx/" +
+			txHash,
+		};
+	} catch (error) {
+		return {
+		success: false,
+		status: "😥 Something went wrong: " + error.message,
+		};
+	}
 };
